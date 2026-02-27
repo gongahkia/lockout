86c04a94c02f9ce8aefac6809c2ebbdf46d969df
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-02-27 10:07:26 +0800

    infra: XCUITest target LockOut-macOSUITests in project.yml

---
+++ LockOut-macOSUITests/LockOutUITests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+final class LockOutUITests: XCTestCase {
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launchArguments = ["--uitesting"]
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+        app.terminate()
+    }
+}
